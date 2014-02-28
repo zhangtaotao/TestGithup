8115f6704baec7697d867c7be250f03f1860a589
zhangtaotao <741221187@qq.com>
2014-02-28 15:41:47 +0800

    add new file

--- TestGithup/AppDelegate.m
+++ TestGithup/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @implementation AppDelegate
 
@@ -19,6 +20,10 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    MainViewController * main = [[MainViewController alloc] init];
+    [self.window setRootViewController:main];
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ TestGithup/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  TestGithup
+//
+//  Created by Tony on 14-2-28.
+//  Copyright (c) 2014年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
